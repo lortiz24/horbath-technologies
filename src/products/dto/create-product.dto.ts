c3373a97fd7cedd6503d9694a12b92a7f7288426
lortiz24 <ortizfrancoluisjavier@gmail.com>
2025-01-14 20:34:51 -0500

    Entidad de producto DTO de producto y conexión con DB

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,17 @@
-export class CreateProductDto {}
+import { IsDecimal, IsInt, IsOptional, IsString, Min } from "class-validator";
+
+export class CreateProductDto {
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsDecimal()
+  price: number;
+
+  @IsInt()
+  @Min(0)
+  quantity: number;
+}
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,31 @@
-export class Product {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  name: string;
+
+  @Column({ type: 'text', nullable: true })
+  description?: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2, nullable: false })
+  price: number;
+
+  @Column({ type: 'int', nullable: false })
+  quantity: number;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
